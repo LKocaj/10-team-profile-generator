4340f315ffc978351f81b3c9d072527668dd40a7
Lawrence <lawrencecaj@gmail.com>
2022-06-23 17:41:00 -0400

    misspelled my own name

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -2,7 +2,7 @@ const Manager = require("../lib/Manager");
 
 describe("Manager class constructor", () => {
   it("New manager creation", () => {
-    const teamMember = new Manager("Lawrene", "1", "lawrencecaj@gmail.com", "1337");
+    const teamMember = new Manager("Lawrence", "1", "lawrencecaj@gmail.com", "1337");
     expect(teamMember.name).toEqual("Lawrence");
     expect(teamMember.id).toEqual("1");
     expect(teamMember.email).toEqual("lawrencecaj@gmail.com");
